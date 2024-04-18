35a4aba0d5a1799108f07d03b0b3b5fc6039432c
Sonali Kumari <guptasonalee88@gmail.com>
2024-04-18 13:24:04 +0530

    reports data for the data written and so description of file

--- components/dummy.js
+++ components/dummy.js
@@ -3,8 +3,19 @@ const reportsData = [
         id :1,
         date : "2024-04-18" ,//today's date
         reportName : "Report 1.csv",
-        downloadLink : ""
+        downloadLink : "reports-dialog\src\app\Report 1.csv"
 
 
-    }
+    },
+    {
+        id :2,
+        date : "2024-04-17" ,
+        reportName : "Report 2.xlsx",
+        downloadLink : "reports-dialog\src\app\Report 2.xlsx"
+
+
+
+    },
+
+
 ]
